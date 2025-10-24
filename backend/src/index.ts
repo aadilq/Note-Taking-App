689e64b99abc0899848a4318c26663fc62d089f2
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-10-23 22:27:19 -0400

    test out express backend

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, TypeScript Express!');
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on Port ${PORT}`)
+})
+
